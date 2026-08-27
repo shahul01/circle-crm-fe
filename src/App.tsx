afaa617b8340052c4e4dd08ffca93e4ed5160210
shahul01 <shahulhammeed01@gmail.com>
2026-08-27 20:29:15 +0530

    refactor: formmated tsx and css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      Circle CRM
-    </div>
-  )
+  return <div>Circle CRM</div>;
 }
 
-export default App
+export default App;
